fed0b7ee5f58e5678579ed0e693dbb28cf103c6b
hector toral <hectortoraltrabajos@gmail.com>
2022-09-29 14:06:58 +0200

    database version

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import prisma from 'prisma/prisma';
 
-export default function handler(req, res) {
-  res.status(200).json({name: 'John Doe'});
+export default async function handler(req, res) {
+  const users = await prisma.user.findMany();
+  res.status(200).json(users);
 }
